24c0ae84e5cc52cb6a7ab2bb136666891fdcfdb8
kenziew <mswhite2@buffalo.edu>
2019-05-01 14:43:49 +0000

    test2

--- keras-perceptron/perceptron-single.py
+++ keras-perceptron/perceptron-single.py
@@ -1,8 +1,8 @@
 from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Flatten
+from keras.models import Sequential # meaning each layer feeds into next layer
+from keras.layers import Dense, Flatten # 2 types of layers
 
-import wandb
+import wandb #allows us to see results as we train
 from wandb.keras import WandbCallback
 
 # logging code
@@ -12,17 +12,18 @@ config = run.config
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-is_five_train = y_train == 5
+is_five_train = y_train == 5 # 0 when not 5, and 1 when it is a 5
 is_five_test = y_test == 5
 labels = ["Not Five", "Is Five"]
 
 img_width = X_train.shape[1]
 img_height = X_train.shape[2]
 
-# create model
+# create model # first shallow learning model
 model = Sequential()
-model.add(Flatten(input_shape=(img_width, img_height)))
-model.add(Dense(1))
+model.add(Flatten(input_shape=(img_width, img_height))) # add intitial flattened layer. takes the 2D 28 by 28 pixels and turns it into 784 lenght array
+model.add(Dense(1)) # dense bc densely connected to previous layer. every input has learned weight from inputs of previously layer. 1 is because we want 1 output. if 2 it would output 2 numbers. so this is a single perceptron. so 1 means you are ouputting the 'schematic of rosenblatts perceptron
+# shape of data after dense 1 is just 1 
 model.compile(loss='mse', optimizer='adam',
               metrics=['accuracy'])
 
